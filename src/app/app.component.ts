6cc561f9631f2fb78b4ae0611a2a0aada3ba7dae
Saul Martinez <78871@alumnos.utleon.edu.mx>
2025-10-13 20:39:50 -0600

    Rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DistanciaComponent } from "./formularios/distancia/distancia.component";
-import { MultiplicacionComponent } from './formularios/multiplicacion/multiplicacion.component';
-import { AporBComponent } from './formularios/apor-b/apor-b.component';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from "./navbar/navbar.component";
+// import { DistanciaComponent } from "./formularios/distancia/distancia.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DistanciaComponent, MultiplicacionComponent, AporBComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularSegundo01';
+
+    ngOnInit(): void {
+    initFlowbite();
+  }
 }
+
+
