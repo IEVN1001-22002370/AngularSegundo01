747b65b0b056fe5f346713c50704c7dc1de3ca89
Omar Castellanos <omar101917@outlook.com>
2025-12-01 21:13:56 -0600

    test-simulador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,9 +11,24 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./utl/utl.routes').then((m) => m.default),
   },
+  {
+    path: 'proyectoLudix/vista-test',
+    loadComponent: () => 
+      import('./proyectoLudix/vista-test/vista-test.component').then(m => m.VistaTestComponent)
+  },
+  {
+  path: 'proyectoLudix/simulador',
+  loadComponent: () => 
+    import('./proyectoLudix/vista-simulador/vista-simulador.component').then(m => m.VistaSimuladorComponent)
+  },
   // {
   //   path: 'formularios',
   //   loadChildren: () =>
   //     import('./formularios/formularios.routes').then((m) => m.default),
   // },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
 ];
--- src/app/proyectoLudix/ludix.routes.ts
+++ src/app/proyectoLudix/ludix.routes.ts
@@ -14,6 +14,10 @@ export default [
     loadComponent: () => import('./vista-sign-up/vista-sign-up.component')
   },
   {
+   path: 'test',
+    loadComponent: () => import('./vista-test/vista-test.component')
+  },
+  {
     path: 'perfil',
     loadComponent: () => import('./vista-perfil/vista-perfil.component')
       .then(m => m.VistaPerfilComponent)
