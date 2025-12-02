804b1d4a26b089d4f956516f3d6d14a4e3c22ffd
EduardoJacinto20 <eduardojacintomunoz@gmail.com>
2025-12-01 20:55:51 -0600

    cambios Lalo

--- src/app/proyectoLudix/vista-sign-up/vista-sign-up.component.ts
+++ src/app/proyectoLudix/vista-sign-up/vista-sign-up.component.ts
@@ -1,45 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-vista-sign-up',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [],
   templateUrl: './vista-sign-up.component.html',
-  styleUrls: ['./vista-sign-up.component.css']
+  styleUrl: './vista-sign-up.component.css'
 })
 export class VistaSignUpComponent {
-  nombre = '';
-  correo = '';
-  contraseña = '';
-  cargando = false;
 
-  constructor(private http: HttpClient) {}
-
-  registrar() {
-    if (!this.nombre || !this.correo || !this.contraseña) {
-      alert('Completa todos los campos');
-      return;
-    }
-
-    this.cargando = true;
-    this.http.post('http://localhost:5000/signup', {
-      nombre: this.nombre,
-      correo: this.correo,
-      contraseña: this.contraseña
-    }).subscribe({
-      next: (res: any) => {
-        alert(res.mensaje);
-        this.cargando = false;
-      },
-      error: (err) => {
-        console.error('Error:', err);
-        alert('Error en el registro');
-        this.cargando = false;
-      }
-    });
-  }
-  
 }
