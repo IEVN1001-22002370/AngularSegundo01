7497f44e52802102badcb57e54ab75213d39e09d
Saul Martinez <78871@alumnos.utleon.edu.mx>
2025-12-02 18:17:52 -0600

    tiendaLudixActualizado

--- src/app/proyectoLudix/vista-login/vista-login.component.ts
+++ src/app/proyectoLudix/vista-login/vista-login.component.ts
@@ -1,41 +1,41 @@
 import { Component } from '@angular/core';
 import { ProyectoapiService } from '../proyectoapi.service';
 import { Router } from '@angular/router';
-
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vista-login',
   templateUrl: './vista-login.component.html',
   styleUrl: './vista-login.component.css',
   standalone: true,
+  imports: [FormsModule]   // ← NECESARIO PARA ngModel
 })
 export class VistaLoginComponent {
 
-  usuario: string = '';
-  contrasena: string = '';
-  errorMsg: string = '';
+  usuario: string = "";
+  contrasena: string = "";
+  errorMsg: string = "";
 
   constructor(private api: ProyectoapiService, private router: Router) {}
 
   onSubmit() {
+
     if (!this.usuario || !this.contrasena) {
-      this.errorMsg = 'Completa todos los campos';
+      this.errorMsg = "Completa todos los campos";
       return;
     }
 
     const datos = {
-      usuario: this.usuario,
-      contrasenia: this.contrasena,
+      usuario: this.usuario,          // aquí va el correo realmente
+      contrasenia: this.contrasena
     };
 
     this.api.login(datos).subscribe((resp: any) => {
       if (resp.exito) {
-        // Guardamos sesión
-        localStorage.setItem('usuario', JSON.stringify(resp.usuario));
 
-        // Redirigir al home / perfil
+        localStorage.setItem("usuario", JSON.stringify(resp.usuario));
         this.router.navigate(['/proyectoLudix/perfil']);
+
       } else {
         this.errorMsg = resp.mensaje;
       }
